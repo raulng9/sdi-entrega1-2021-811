fb59d44c0ac1e715d22209e891f69b5f5254b6d4
raulng9 <salprez4@gmail.com>
2021-03-09 13:32:29 +0100

    Commit inicial con los materiales necesarios para empezar el proyecto

---
+++ src/main/java/com/mywallapop/MyWallapopSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mywallapop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWallapopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWallapopSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mywallapop/MyWallapopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywallapop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWallapopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
